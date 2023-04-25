558295d23f9cf4e3d0e40f1e774fe8a9f71b893c
dhinadk28 <103361324+dhinadk28@users.noreply.github.com>
2023-04-25 22:54:23 +0530

    add

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,11 @@ const errorMiddleware = require('./middlewares/error');
 const cookieParser = require('cookie-parser')
 const path = require('path')
 const dotenv = require('dotenv');
+const cors = require('cors')
 dotenv.config({path:path.join(__dirname,"config/config.env")});
 
 
+app.use(cors({origin:'*'}))
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname,'uploads') ) )
